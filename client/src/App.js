f7b84cd2f49113621cb77199d93e29c77429a0bd
koskrit <koskritm@gmail.com>
2021-02-03 02:16:58 +0200

    Added React client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import AppBar from "./components/AppBar"
+
+function App() {
+  return (
+    <div className="App">
+      <AppBar/>
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
