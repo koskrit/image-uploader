aa9f0373cbe52e5b5d4d414becc57bff14d454d1
koskrit <koskritm@gmail.com>
2021-02-03 22:13:28 +0200

    re-initialized react-app , added filepond

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,36 @@
-import './App.css';
-import AppBar from "./components/AppBar"
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-function App() {
+// Import React FilePond
+import { FilePond, File, registerPlugin } from 'react-filepond'
+
+// Import FilePond styles
+import 'filepond/dist/filepond.min.css'
+
+// Import the Image EXIF Orientation and Image Preview plugins
+// Note: These need to be installed separately
+// `npm i filepond-plugin-image-preview filepond-plugin-image-exif-orientation --save`
+import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation'
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css'
+
+// Register the plugins
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview)
+
+// Our app
+export default function App() {
+  const [files, setFiles] = useState([])
   return (
     <div className="App">
-      <AppBar/>
-      <h1>hello world</h1>
+      <FilePond
+        files={files}
+        onupdatefiles={setFiles}
+        allowMultiple={true}
+        maxFiles={3}
+        server="/api"
+        name="files" /* sets the file input name, it's filepond by default */
+        labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+      />
     </div>
-  );
+  )
 }
-
-export default App;
