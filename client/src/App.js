1942593f34787f67ea273b083fb2b0e527e69b21
koskrit <koskritm@gmail.com>
2021-02-04 03:00:00 +0200

    adds material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,14 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-
-// Import React FilePond
 import { FilePond, File, registerPlugin } from 'react-filepond'
 
-// Import FilePond styles
 import 'filepond/dist/filepond.min.css'
 
-// Import the Image EXIF Orientation and Image Preview plugins
-// Note: These need to be installed separately
-// `npm i filepond-plugin-image-preview filepond-plugin-image-exif-orientation --save`
 import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation'
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css'
 
+import AppBar from "./Components/appBar"
 // Register the plugins
 registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview)
 
@@ -22,6 +17,7 @@ export default function App() {
   const [files, setFiles] = useState([])
   return (
     <div className="App">
+      <AppBar/>
       <FilePond
         files={files}
         onupdatefiles={setFiles}
