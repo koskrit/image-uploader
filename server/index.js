68e25a6f7848c399228bed3c517f769014afe3f1
koskrit <koskritm@gmail.com>
2021-02-12 06:18:40 +0200

    adds endpoint for deleting files

--- server/index.js
+++ server/index.js
@@ -1,42 +1,49 @@
-
 const express = require("express");
-const path = require('path')
+const path = require("path");
+const fs = require("fs");
 
 const cors = require("cors");
-const multer = require('multer')
-
+const multer = require("multer");
+const { fstat } = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 const storage = multer.diskStorage({
-  destination: './uploads/',
-  filename: function(req, file, cb){
+   destination: "./uploads/",
+   filename: function (req, file, cb) {
       cb(null, file.originalname);
-  }
-  
+   },
 });
 
 app.use(cors());
 
-const upload = multer({ dest: 'uploads/' ,storage:storage})
+const upload = multer({ dest: "uploads/", storage: storage });
 
+app.get("/uploads/:name", (req, res) => {
+   res.sendFile(path.join(__dirname, "uploads", req.params.name));
+});
 
-app.get("/uploads/:name",(req,res) => {
-res.sendFile(path.join(__dirname,"uploads", req.params.name))
+app.post("/post", upload.any({ name: "image.png" }), async (req, res) => {
+   console.log(req.files[0].originalname);
 
-})
-
-
-app.post("/post",upload.any({name:"image.png"}),  async (req, res) => {
-  console.log(req.files[0].originalname);
-
-  await res.send('image saved')
+   await res.send("image saved");
+});
+app.get("/delete",  (req, res) => {
+   fs.readdir("uploads", (err, results) => {
+      results.forEach((file) => {
+         fs.unlink("uploads/" + file, () => {
+            console.log("file " + file + " deleted");
+         });
+      });
+   });
 });
 
 app.listen(PORT, () => {
-  console.log("listening on http://localhost:4000");
+   console.log("listening on http://localhost:4000");
 });
 
-// to deploy create a vercel.json (delete previous .gitignore, .vercel(Folder) 
-   
+// to deploy create a vercel.json (delete previous .gitignore, .vercel(Folder)
+
+
+
